cc4ac539cec2f692101b4d4277d08c610a86b886
Дмитрий <region29@bk.ru>
2023-02-10 13:00:21 +0300

    проверти дз

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     }
 
     public static void task1() {
-        System.out.println("задача 1 ");
+        System.out.println("задача  1 ");
         /* С помощью цикла for выведите в консоль все числа от 1 до 10.*/
         for (int i = 0; i <= 10; i = i + 1) {
             System.out.println(i);
